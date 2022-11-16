e67b017f3d59d0dc2762402a08083534466ca178
alexandr-drozdov <adrozdov24@gmail.com>
2022-11-16 17:36:36 +0300

    Separate scene creation from viewer creation

--- src/index.js
+++ src/index.js
@@ -84,114 +84,136 @@ function getBulgeCurvePoints(startPoint, endPoint, bulge, segments) {
  * @constructor
  */
 export function Viewer(data, parent, width, height, font) {
+	var drawer = new Drawer(data, font);
+	this.scene = drawer.createScene(data);
 
-    createLineTypeShaders(data);
+	if (parent) {
+		createViewer(this.scene, parent, width, height);
+	}
 
-    var scene = new THREE.Scene();
+	function createViewer(scene, parent, width, height) {
+		width = width || parent.clientWidth;
+		height = height || parent.clientHeight;
+		var aspectRatio = width / height;
 
-    // Create scene from dxf object (data)
-    var i, entity, obj, min_x, min_y, min_z, max_x, max_y, max_z;
-    var dims = {
-        min: { x: 0, y: 0, z: 0 },
-        max: { x: 0, y: 0, z: 0 }
-    };
-    for (i = 0; i < data.entities.length; i++) {
-        entity = data.entities[i];
-        obj = drawEntity(entity, data);
+		var upperRightCorner = { x: dims.max.x, y: dims.max.y };
+		var lowerLeftCorner = { x: dims.min.x, y: dims.min.y };
 
-        if (obj) {
-            var bbox = new THREE.Box3().setFromObject(obj);
-            if (isFinite(bbox.min.x) && (dims.min.x > bbox.min.x)) dims.min.x = bbox.min.x;
-            if (isFinite(bbox.min.y) && (dims.min.y > bbox.min.y)) dims.min.y = bbox.min.y;
-            if (isFinite(bbox.min.z) && (dims.min.z > bbox.min.z)) dims.min.z = bbox.min.z;
-            if (isFinite(bbox.max.x) && (dims.max.x < bbox.max.x)) dims.max.x = bbox.max.x;
-            if (isFinite(bbox.max.y) && (dims.max.y < bbox.max.y)) dims.max.y = bbox.max.y;
-            if (isFinite(bbox.max.z) && (dims.max.z < bbox.max.z)) dims.max.z = bbox.max.z;
-            scene.add(obj);
-        }
-        obj = null;
-    }
+		// Figure out the current viewport extents
+		var vp_width = upperRightCorner.x - lowerLeftCorner.x;
+		var vp_height = upperRightCorner.y - lowerLeftCorner.y;
+		var center = center || {
+			x: vp_width / 2 + lowerLeftCorner.x,
+			y: vp_height / 2 + lowerLeftCorner.y
+		};
 
-    width = width || parent.clientWidth;
-    height = height || parent.clientHeight;
-    var aspectRatio = width / height;
+		// Fit all objects into current ThreeDXF viewer
+		var extentsAspectRatio = Math.abs(vp_width / vp_height);
+		if (aspectRatio > extentsAspectRatio) {
+			vp_width = vp_height * aspectRatio;
+		} else {
+			vp_height = vp_width / aspectRatio;
+		}
 
-    var upperRightCorner = { x: dims.max.x, y: dims.max.y };
-    var lowerLeftCorner = { x: dims.min.x, y: dims.min.y };
+		var viewPort = {
+			bottom: -vp_height / 2,
+			left: -vp_width / 2,
+			top: vp_height / 2,
+			right: vp_width / 2,
+			center: {
+				x: center.x,
+				y: center.y
+			}
+		};
 
-    // Figure out the current viewport extents
-    var vp_width = upperRightCorner.x - lowerLeftCorner.x;
-    var vp_height = upperRightCorner.y - lowerLeftCorner.y;
-    var center = center || {
-        x: vp_width / 2 + lowerLeftCorner.x,
-        y: vp_height / 2 + lowerLeftCorner.y
-    };
+		var camera = new THREE.OrthographicCamera(viewPort.left, viewPort.right, viewPort.top, viewPort.bottom, 1, 19);
+		camera.position.z = 10;
+		camera.position.x = viewPort.center.x;
+		camera.position.y = viewPort.center.y;
 
-    // Fit all objects into current ThreeDXF viewer
-    var extentsAspectRatio = Math.abs(vp_width / vp_height);
-    if (aspectRatio > extentsAspectRatio) {
-        vp_width = vp_height * aspectRatio;
-    } else {
-        vp_height = vp_width / aspectRatio;
-    }
+		var renderer = this.renderer = new THREE.WebGLRenderer();
+		renderer.setSize(width, height);
+		renderer.setClearColor(0xfffffff, 1);
 
-    var viewPort = {
-        bottom: -vp_height / 2,
-        left: -vp_width / 2,
-        top: vp_height / 2,
-        right: vp_width / 2,
-        center: {
-            x: center.x,
-            y: center.y
-        }
-    };
+		parent.appendChild(renderer.domElement);
+		parent.style.display = 'block';
 
-    var camera = new THREE.OrthographicCamera(viewPort.left, viewPort.right, viewPort.top, viewPort.bottom, 1, 19);
-    camera.position.z = 10;
-    camera.position.x = viewPort.center.x;
-    camera.position.y = viewPort.center.y;
+		//TODO: Need to make this an option somehow so others can roll their own controls.
+		var controls = new OrbitControls(camera, parent);
+		controls.target.x = camera.position.x;
+		controls.target.y = camera.position.y;
+		controls.target.z = 0;
+		controls.zoomSpeed = 3;
 
-    var renderer = this.renderer = new THREE.WebGLRenderer();
-    renderer.setSize(width, height);
-    renderer.setClearColor(0xfffffff, 1);
+		//Uncomment this to disable rotation (does not make much sense with 2D drawings).
+		//controls.enableRotate = false;
 
-    parent.appendChild(renderer.domElement);
-    parent.style.display = 'block';
+		this.render = function () { renderer.render(scene, camera) };
+		controls.addEventListener('change', this.render);
+		this.render();
+		controls.update();
 
-    //TODO: Need to make this an option somehow so others can roll their own controls.
-    var controls = new OrbitControls(camera, parent);
-    controls.target.x = camera.position.x;
-    controls.target.y = camera.position.y;
-    controls.target.z = 0;
-    controls.zoomSpeed = 3;
+		this.resize = function (width, height) {
+			var originalWidth = renderer.domElement.width;
+			var originalHeight = renderer.domElement.height;
 
-    //Uncomment this to disable rotation (does not make much sense with 2D drawings).
-    //controls.enableRotate = false;
-
-    this.render = function () { renderer.render(scene, camera) };
-    controls.addEventListener('change', this.render);
-    this.render();
-    controls.update();
-
-    this.resize = function (width, height) {
-        var originalWidth = renderer.domElement.width;
-        var originalHeight = renderer.domElement.height;
-
-        var hscale = width / originalWidth;
-        var vscale = height / originalHeight;
+			var hscale = width / originalWidth;
+			var vscale = height / originalHeight;
 
 
-        camera.top = (vscale * camera.top);
-        camera.bottom = (vscale * camera.bottom);
-        camera.left = (hscale * camera.left);
-        camera.right = (hscale * camera.right);
+			camera.top = (vscale * camera.top);
+			camera.bottom = (vscale * camera.bottom);
+			camera.left = (hscale * camera.left);
+			camera.right = (hscale * camera.right);
 
-        //        camera.updateProjectionMatrix();
+			//        camera.updateProjectionMatrix();
 
-        renderer.setSize(width, height);
-        renderer.setClearColor(0xfffffff, 1);
-        this.render();
-    };
+			renderer.setSize(width, height);
+			renderer.setClearColor(0xfffffff, 1);
+			this.render();
+		};
+	}
+}
+
+/**
+ * Drawer class for a dxf object.
+ * @param {Object} data - the dxf object
+ * @param {Object} font - a font loaded with THREE.FontLoader
+ * @constructor
+ */
+export function Drawer(data, font) {
+    
+	this.createScene = createScene;
+
+	function createScene(data) {
+		createLineTypeShaders(data);
+
+		var scene = new THREE.Scene();
+
+		// Create scene from dxf object (data)
+		var i, entity, obj, min_x, min_y, min_z, max_x, max_y, max_z;
+		var dims = {
+			min: { x: 0, y: 0, z: 0 },
+			max: { x: 0, y: 0, z: 0 }
+		};
+		for (i = 0; i < data.entities.length; i++) {
+			entity = data.entities[i];
+			obj = drawEntity(entity, data);
+
+			if (obj) {
+				var bbox = new THREE.Box3().setFromObject(obj);
+				if (isFinite(bbox.min.x) && (dims.min.x > bbox.min.x)) dims.min.x = bbox.min.x;
+				if (isFinite(bbox.min.y) && (dims.min.y > bbox.min.y)) dims.min.y = bbox.min.y;
+				if (isFinite(bbox.min.z) && (dims.min.z > bbox.min.z)) dims.min.z = bbox.min.z;
+				if (isFinite(bbox.max.x) && (dims.max.x < bbox.max.x)) dims.max.x = bbox.max.x;
+				if (isFinite(bbox.max.y) && (dims.max.y < bbox.max.y)) dims.max.y = bbox.max.y;
+				if (isFinite(bbox.max.z) && (dims.max.z < bbox.max.z)) dims.max.z = bbox.max.z;
+				scene.add(obj);
+			}
+			obj = null;
+		}
+		return scene;
+	}
 
     function drawEntity(entity, data) {
         var mesh;
@@ -761,6 +783,14 @@ export function Viewer(data, parent, width, height, font) {
 
 }
 
+class DxfObjectLoader extends THREE.Loader {
+	constructor(manager) {
+		super(manager);
+	}
+
+	load(dataObject, onLoad, onProgress, onError) {
+	}
+}
 
 // Show/Hide helpers from https://plainjs.com/javascript/effects/hide-or-show-an-element-42/
 // get the default display style of an element
